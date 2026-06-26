eaebac2a732bb661304599cf077b152753ff4961
sppal912 <souvikpal9123370318@gmail.com>
2026-06-26 15:38:49 +0000

    react update

--- 01basic/src/main.jsx
+++ 01basic/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './style.css'
+import souvik from './souvik.jsx'
+import Souvik from './souvik.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Souvik />
   </StrictMode>,
 )
---
+++ 01basic/src/souvik.jsx
@@ -0,0 +1,6 @@
+function Souvik() {
+    return (
+        <h1>My name is Souvik pal</h1>
+    )
+}
+export default Souvik
