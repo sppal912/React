ca9b62b20ccc53a688f9aaee9436d8083b9a9c61
sppal912 <souvikpal9123370318@gmail.com>
2026-07-19 05:12:52 +0000

    login submit with context

--- UserLogin/src/component/Login.jsx
+++ UserLogin/src/component/Login.jsx
@@ -3,18 +3,29 @@ import { useState, useContext } from "react";
 import UserContext from "../context/UserContex";
 
 function Login() {
+    const [error, setError] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const { setUser } = useContext(UserContext);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setUser({ username, password });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!username.trim() || !password.trim()) {
+        setError("Please enter username and password");
+        setTimeout(() => {
+            setError("")
+        },3000)
+        return;
+    }
+    setError("");
+    setUser({ username, password });
+};
 
     return (
         <div>
             <h2>Login</h2>
+            {error && <p style={{ color: "red" }}>{error}</p>}
             <input
                 type="text"
                 value={username}
--- UserLogin/src/component/Profile.jsx
+++ UserLogin/src/component/Profile.jsx
@@ -2,7 +2,10 @@ import { useContext } from "react";
  import UserContext from "../context/UserContex";
   function Profile () {
     const {user} = useContext(UserContext)
-    if(!user) return <div>Please Login</div>
-    return <div>WELCOME {user.username}</div>
+    if(!user || user === ""){
+      return <div>Please Login</div>
+    } else{
+      return <div>WELCOME {user.username}</div>
+    }
   }
   export default Profile;
